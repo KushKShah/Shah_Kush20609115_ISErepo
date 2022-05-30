27562d996d2eff828404dfa5344c850b7c85faf7
Kush <20609115@student.curtin.edu.au>
2022-05-30 19:41:53 +0800

    All Converts Similar to ConvertHoursToMinutes

--- Codes/Category2c.java
+++ Codes/Category2c.java
@@ -287,4 +287,82 @@ public class Category2c
         finalString += value + " Hours in Minutes is " + stringValue + " Minutes";
         return finalString;
     }
+    public static String ConvertMinutesToHours(String value)
+    {
+        double finalValue = StringToDouble(value);
+        finalValue = finalValue/60;
+        String stringValue = TwoDecimalPlaces(finalValue);
+        String finalString = "";
+        if (finalValue == 0)
+        {
+            if (value.equals("0") == false)
+            {
+                System.out.println("|-------------|");
+                System.out.println("|Invalid Value|");
+                System.out.println("|-------------|");
+                finalString += "|-------------|";
+                finalString += "\n";
+                finalString += "|Invalid Value|";
+                finalString += "\n";
+                finalString += "|-------------|";
+                finalString += "\n";
+            }
+        }
+        System.out.println(value + " Minutes in Hours is " + stringValue + " Hours");
+        System.out.println();
+        finalString += value + " Minutes in Hours is " + stringValue + " Hours";
+        return finalString;
+    }
+    public static String ConvertMinutesToSeconds(String value)
+    {
+        double finalValue = StringToDouble(value);
+        finalValue = finalValue*60;
+        String stringValue = TwoDecimalPlaces(finalValue);
+        String finalString = "";
+        if (finalValue == 0)
+        {
+            if (value.equals("0") == false)
+            {
+                System.out.println("|-------------|");
+                System.out.println("|Invalid Value|");
+                System.out.println("|-------------|");
+                finalString += "|-------------|";
+                finalString += "\n";
+                finalString += "|Invalid Value|";
+                finalString += "\n";
+                finalString += "|-------------|";
+                finalString += "\n";
+            }
+        }
+        System.out.println(value + " Minutes in Seconds is " + stringValue + " Seconds");
+        System.out.println();
+        finalString += value + " Minutes in Seconds is " + stringValue + " Seconds";
+        return finalString;
+    }
+    public static String ConvertSecondsToMinutes(String value)
+    {
+        double finalValue = StringToDouble(value);
+        finalValue = finalValue/60;
+        String stringValue = TwoDecimalPlaces(finalValue);
+        String finalString = "";
+        if (finalValue == 0)
+        {
+            if (value.equals("0") == false)
+            {
+                System.out.println("|-------------|");
+                System.out.println("|Invalid Value|");
+                System.out.println("|-------------|");
+                finalString += "|-------------|";
+                finalString += "\n";
+                finalString += "|Invalid Value|";
+                finalString += "\n";
+                finalString += "|-------------|";
+                finalString += "\n";
+            }
+        }
+        System.out.println(value + " Seconds in Minutes is " + stringValue + " Minutes");
+        System.out.println();
+        finalString += value + " Seconds in Minutes is " + stringValue + " Minutes";
+        return finalString;
+    }
 }
