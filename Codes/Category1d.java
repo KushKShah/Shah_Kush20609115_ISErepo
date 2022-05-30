fb329a27bc754f125449e8c706f645fcc5850228
Kush <20609115@student.curtin.edu.au>
2022-05-30 18:31:28 +0800

    Main

---
+++ Codes/Category1d.java
@@ -0,0 +1,18 @@
+//
+// 20609115
+// Kush Shah
+//
+
+import java.util.*;
+import java.io.*;
+
+public class Category1d {
+    public static void main(String[] args)
+    {
+        String outputFileName = "1dResults.txt";
+        System.out.println();
+        System.out.println("Welcome to the program where Numbers are Removed and you can choose whether you want to convert the String to Upper Case or Lower Case");
+        System.out.println();
+        ReadInFileMenu(outputFileName);
+    }
+}
