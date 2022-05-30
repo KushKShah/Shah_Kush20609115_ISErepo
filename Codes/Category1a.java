73b084133cc0d98201e5733f2513a4689d770c2d
Kush <20609115@student.curtin.edu.au>
2022-05-30 16:46:20 +0800

    Category1a(ReadInFileMenu) Commit

--- Codes/Category1a.java
+++ Codes/Category1a.java
@@ -16,4 +16,47 @@ public class Category1a
         System.out.println();
         ReadInFileMenu(outputFileName);
     }
+    public static void ReadInFileMenu(String outputFileName)
+    {
+        boolean running = true;
+        String inputFileName = "";
+        Scanner sc = new Scanner(System.in);
+        while
+        {
+            
+            System.out.println("Would you like to Read in a file or enter a string?");
+            System.out.println("If you would like to Read in a file type 'Yes' if not then type 'No'");
+            System.out.print("Enter you Choice: ");
+            String readInFile = sc.next();
+            readInFile += sc.nextLine();
+            readInFile = readInFile.toLowerCase();
+            System.out.println();
+            if (readInFile.equals("yes") == false)
+            {
+                if (readInFile.equals("no") == true)
+                {
+                    CaseMenu("no", inputFileName, outputFileName);
+                    running = false;
+                }
+                else if (readInFile.equals("no") == false)
+                {
+                    System.out.println();
+                    System.out.println("Invalid String Input");
+                    System.out.println("Please Input Either 'Yes' or 'No'");
+                    System.out.println();
+                    running = true;
+                }
+            }
+            else if (readInFile.equals("yes") == true)
+            {
+                System.out.println();
+                System.out.println("You selected to input a File Name");
+                System.out.println("Please input the name of the file with the file type (e.g. File.txt)");
+                System.out.print("Enter the Name of The File: ");
+                inputFileName = sc.next();
+                System.out.println();
+                CaseMenu("yes", inputFileName, outputFileName);
+                running = false;
+        }
+    }
 }
