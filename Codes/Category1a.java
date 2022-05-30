773ed06d1af5f6eaeed5d89888e6e51e4dd55878
Kush <20609115@student.curtin.edu.au>
2022-05-30 16:41:56 +0800

    Category1a(main) Commit

---
+++ Codes/Category1a.java
@@ -0,0 +1,19 @@
+//
+// 20609115
+// Kush Shah
+//
+
+import java.util.*;
+import java.io.*;
+
+public class Category1a
+{
+    public static void main(String[] args)
+    {
+        String outputFileName = "1aResults.txt";
+        System.out.println();
+        System.out.println("Welcome to the Program to turn Strings to Upper Case or Lower Case");
+        System.out.println();
+        ReadInFileMenu(outputFileName);
+    }
+}
