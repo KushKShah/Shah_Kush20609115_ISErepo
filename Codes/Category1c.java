fa3eba1107fd9fbc74b5f12fb1c90e64510d5530
Kush <20609115@student.curtin.edu.au>
2022-05-30 18:15:38 +0800

    Methods Similar to other javas

---
+++ Codes/Category1c.java
@@ -0,0 +1,74 @@
+//
+// 20609115
+// Kush Shah
+//
+
+import java.util.*;
+import java.io.*;
+
+public class Category1c {
+    public static void main(String[] args)
+    {
+        String outputFileName = "1cResults.txt";
+        System.out.println();
+        System.out.println("Welcome to the program where you can see if the value give is a valid value or not");
+        System.out.println();
+        ReadInFileMenu(outputFileName);
+    }
+    public static void ReadInFileMenu(String outputFileName)
+    {
+        boolean running = true;
+        String inputFileName = "n/a";
+        Scanner sc = new Scanner(System.in);
+        while(running == true)
+        {
+            System.out.println("Would you like to Read in a file or enter a string?");
+            System.out.println("If you would like to Read in a file type 'Yes' if not then type 'No'");
+            System.out.print("Enter you Choice: ");
+            String readInFile = sc.next();
+            readInFile += sc.nextLine();
+            readInFile = readInFile.toLowerCase();
+            System.out.println();
+            if (readInFile.equals("yes") == false)
+            {
+                if (readInFile.equals("no") == true)
+                {
+                    NoMenu(outputFileName);
+                    running = false;
+                }
+                else if (readInFile.equals("no") == false)
+                {
+                    System.out.println();
+                    System.out.println("Invalid String Input");
+                    System.out.println("Please Input Either 'Yes' or 'No'");
+                    System.out.println();
+                    running = true;
+                }
+            }
+            else if (readInFile.equals("yes") == true)
+            {
+                System.out.println();
+                System.out.println("You selected to input a File Name");
+                System.out.println("Please input the name of the file with the file type (e.g. File.txt)");
+                System.out.print("Enter the Name of The File: ");
+                inputFileName = sc.next();
+                System.out.println();
+                ReadFile(inputFileName, outputFileName);
+                running = false;
+            }
+        }
+        sc.close();
+    }
+    public static void NoMenu(String outputFileName)
+    {
+        Scanner scone = new Scanner(System.in);
+        String inputValue = "";
+        System.out.println("Enter the Value you would like to use to check if it is a Valid Value: ");
+        inputValue = scone.next();
+        inputValue += scone.nextLine();
+        System.out.println();
+        String finalValue = CheckForValidNumber(inputValue);
+        WriteToFile(outputFileName, finalValue);
+        scone.close();
+    }
+}
