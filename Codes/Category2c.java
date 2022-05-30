3435f7d696369cd5eb85495634180d939bcfb7e1
Kush <20609115@student.curtin.edu.au>
2022-05-30 19:18:09 +0800

    CaseMenu

--- Codes/Category2c.java
+++ Codes/Category2c.java
@@ -62,3 +62,129 @@ public class Category2c
         }
         sc.close();
     }
+    public static void CaseMenu(String inputStatus, String inputFileName, String outputFileName)
+    {
+        boolean menu = true;
+        String choice = "";
+        String value = "";
+        String finalString = "";
+        Scanner scone = new Scanner(System.in);
+        if (inputStatus.equals("yes") == true)
+        {
+            finalString = ReadFile(inputFileName, choice);
+            if (finalString.equals("n/a") == true)
+            {
+                System.out.println();
+                System.out.println("|----------------|");
+                System.out.println("|Please Try Again|");
+                System.out.println("|----------------|");
+                System.out.println();
+                readInFileMenu(outputFileName);
+                menu = false;
+            }
+            else
+            {
+                menu = true;
+            }
+        }
+        do
+        {
+            System.out.println();
+            System.out.println("Select what you want to convert to");
+            System.out.println("Choices you have: ");
+            System.out.println("> (1) Change Hours to Minutes");
+            System.out.println("> (2) Change Minutes to Hours");
+            System.out.println("> (3) Change Minutes to Seconds");
+            System.out.println("> (4) Change Seconds to Minutes");
+            System.out.println("> (0) Exit the Program");
+            System.out.print("Enter your Choice: ");
+            choice = scone.next();
+            System.out.println();
+            
+            switch(choice)
+            {
+                case "1":
+                    if (inputStatus.equals("yes") == true)
+                    {
+                        finalString = ReadFile(inputFileName, choice);
+                        WriteToFile(outputFileName, finalString);
+                    }
+                    else if (inputStatus.equals("no") == true)
+                    {
+                        System.out.println("Enter the Number in Hours that you would like to change to Minutes");
+                        value = scone.next();
+                        value += scone.nextLine();
+                        System.out.println();
+                        finalString = ConvertHoursToMinutes(value);
+                        WriteToFile(outputFileName, finalString);
+                    }
+                    menu = false;
+                    break;
+                case "2":
+                    if (inputStatus.equals("yes") == true)
+                    {
+                        finalString = ReadFile(inputFileName, choice);
+                        WriteToFile(outputFileName, finalString);
+                    }
+                    else if (inputStatus.equals("no") == true)
+                    {
+                        System.out.println("Enter the Number in Minutes that you would like to change to Hours");
+                        value = scone.next();
+                        value += scone.nextLine();
+                        System.out.println();
+                        finalString += ConvertMinutesToHours(value);
+                        WriteToFile(outputFileName, finalString);
+                    }
+                    menu = false;
+                    break;
+                case "3":
+                    if (inputStatus.equals("yes") == true)
+                    {
+                        finalString = ReadFile(inputFileName, choice);
+                        WriteToFile(outputFileName, finalString);
+                    }
+                    else if (inputStatus.equals("no") == true)
+                    {
+                        System.out.println("Enter the Number in Minutes that you would like to change to Seconds");
+                        value = scone.next();
+                        value += scone.nextLine();
+                        System.out.println();
+                        finalString += ConvertMinutesToSeconds(value);
+                        WriteToFile(outputFileName, finalString);
+                    }
+                    menu = false;
+                    break;
+                case "4":
+                    if (inputStatus.equals("yes") == true)
+                    {
+                        finalString = ReadFile(inputFileName, choice);
+                        WriteToFile(outputFileName, finalString);
+                    }
+                    else if (inputStatus.equals("no") == true)
+                    {
+                        System.out.println("Enter the Number in Seconds that you would like to change to Minutes");
+                        value = scone.next();
+                        value += scone.nextLine();
+                        System.out.println();
+                        finalString += ConvertSecondsToMinutes(value);
+                        WriteToFile(outputFileName, finalString);
+                    }
+                    menu = false;
+                    break;
+                case "0":
+                    System.out.println("Thank You for using the program");
+                    System.out.println();
+                    menu = false;
+                    break;
+                default:
+                    System.out.println("|------------------------------|");
+                    System.out.println("|Invalid Value Please Try Again|");
+                    System.out.println("|------------------------------|");
+                    System.out.println();
+                    menu = true;
+                    break;
+            }
+        } while (menu == true);
+        scone.close();
+    }
+}
