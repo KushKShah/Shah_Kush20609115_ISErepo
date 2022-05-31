e11eec8e26127c6d14a0c3fc5f20a414e820ec03
Kush <20609115@student.curtin.edu.au>
2022-05-31 12:47:43 +0800

    Changed Category 1b to reduce if statements

--- Codes/Category1b.java
+++ Codes/Category1b.java
@@ -1,6 +1,6 @@
 //
-//
-//
+// 20609115
+//  Kush Shah
 //
 
 import java.util.*;
@@ -134,42 +134,42 @@ public class Category1b {
             ASCIIvalue = (int)arrayChar[i];
             if ((ASCIIvalue >= 48) && (ASCIIvalue <= 57))
             {
+                finalString = "The String: '" + inputString + "'";
+                finalString += "\n";
+                finalString += "|------------------------|";
+                finalString += "\n";
+                finalString += "|Does have a Number in it|";
+                finalString += "\n";
+                finalString += "|------------------------|";
+                finalString += "\n";
+                finalString += "\n";
+                System.out.println("The String: '" + inputString + "'");
+                System.out.println("|------------------------|");
+                System.out.println("|Does have a Number in it|");
+                System.out.println("|------------------------|");
+                System.out.println();
                 i = arrayChar.length - 1;
             }
-        }
-        if ((ASCIIvalue >= 48) && (ASCIIvalue <= 57))
-        {
-            finalString = "The String: '" + inputString + "'";
-            finalString += "\n";
-            finalString += "|------------------------|";
-            finalString += "\n";
-            finalString += "|Does have a Number in it|";
-            finalString += "\n";
-            finalString += "|------------------------|";
-            finalString += "\n";
-            finalString += "\n";
-            System.out.println("The String: '" + inputString + "'");
-            System.out.println("|------------------------|");
-            System.out.println("|Does have a Number in it|");
-            System.out.println("|------------------------|");
-            System.out.println();
-        }
-        else if ((ASCIIvalue >= 58) && (ASCIIvalue <= 127))
-        {
-            finalString = "The String: '" + inputString + "'";
-            finalString += "\n";
-            finalString += "|----------------------------|";
-            finalString += "\n";
-            finalString += "|Does NOT have a Number in it|";
-            finalString += "\n";
-            finalString += "|----------------------------|";
-            finalString += "\n";
-            finalString += "\n";
-            System.out.println("The String: '" + inputString + "'");
-            System.out.println("|----------------------------|");
-            System.out.println("|Does NOT have a Number in it|");
-            System.out.println("|----------------------------|");
-            System.out.println();
+            else if ((ASCIIvalue >= 58) && (ASCIIvalue <= 127))
+            {
+                if (i == arrayChar.length - 1)
+                {
+                    finalString = "The String: '" + inputString + "'";
+                    finalString += "\n";
+                    finalString += "|----------------------------|";
+                    finalString += "\n";
+                    finalString += "|Does NOT have a Number in it|";
+                    finalString += "\n";
+                    finalString += "|----------------------------|";
+                    finalString += "\n";
+                    finalString += "\n";
+                    System.out.println("The String: '" + inputString + "'");
+                    System.out.println("|----------------------------|");
+                    System.out.println("|Does NOT have a Number in it|");
+                    System.out.println("|----------------------------|");
+                    System.out.println();
+                }
+            }
         }
         return finalString;
     }
